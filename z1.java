bfe89510bf81259b9e5dba3ac3936fea5e256784
Zavaaaat765 <108157254+Zavaaaat765@users.noreply.github.com>
2022-06-25 15:29:43 +0300

    Add files via upload

---
+++ z1.java
@@ -0,0 +1,14 @@
+/* Задание 1
+ Реализовать программу, получающую на вход в качестве аргумента
+ имя человека и выводящую “Hello ” + имя, в противном случае,
+ если параметр не передавался, “Hello world”.
+ */
+public class z1 {
+    public static void main(String args[]) {
+        if (args.length> 0)
+            System.out.println("Hello " + args[0]);
+        else
+            System.out.println("Hello world");
+    }
+}
+
---
+++ z3.java
@@ -0,0 +1,14 @@
+/* Задание 3
+ Передавать в качестве параметров два целочисленных числа. Вывести на экран,
+ как сами значения, так и их сумму. Если количество параметров не равно 2,
+ вывести сообщение “Неверное количество параметров”.
+ */
+public class z3 {
+    public static void main(String args[]) {
+        if (args.length ==  2) {
+            int sum = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+            System.out.println(args[0] + " + " + args[1] + " = " + sum);
+        }
+        else
+            System.out.println("Неверное количество параметров");
+    }
